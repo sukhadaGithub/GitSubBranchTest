e3996e032f0088d457174951d65507f2f0873361
SAMMY <SAMMY@DESKTOP-ATKLMDH>
2024-07-17 23:16:41 +0530

    Script Created by Tester2

---
+++ src/test/java/Tester2/Demo3.java
@@ -0,0 +1,12 @@
+package Tester2;
+
+public class Demo3 {
+
+	public static void main(String[] args)
+	{
+		System.out.println("Script creatd by Tester2");
+		// TODO Auto-generated method stub
+
+	}
+
+}
